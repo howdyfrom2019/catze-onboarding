3b9cc8d7372e27feef07bc093b2256691923954c
howdyfrom2019 <tjd1emd@naver.com>
2023-04-18 15:43:34 +0900

    Fix tailwind with feature-based structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,2 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { RecoilRoot } from "recoil";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  );
-}
+export { default } from '@/features/Landing/LandingPage';

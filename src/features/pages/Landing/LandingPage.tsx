c16500d44a3d12ba72099b78abf544ea0e573b58
howdyfrom2019 <tjd1emd@naver.com>
2023-04-19 14:00:34 +0900

    Add UI of currentTime

--- src/features/pages/Landing/LandingPage.tsx
+++ src/features/pages/Landing/LandingPage.tsx
@@ -1,11 +1,14 @@
 import Card from "@/components/Card";
+import useCurrentTime from "@/hooks/useCurrentTime";
 
 const LandingPage = () => {
+  const cntTime = useCurrentTime();
+
   return (
     <main className="min-h-screen w-screen">
       <article className={'flex flex-col items-center gap-0.5 w-[395px] mx-auto my-0'}>
         <span className={'font-bold-32 text-black pt-8'}>Hi! 🤚 Catze!</span>
-        <span className={'font-bold-20 text-black'}>2022.11.25</span>
+        <span className={'font-bold-20 text-black'}>{cntTime}</span>
         <Card contents={'it\'s test'} isDone={false} />
       </article>
     </main>

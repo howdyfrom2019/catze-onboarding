672ead251e42892d137ad773ab3d8c31ca0f8a5c
howdyfrom2019 <tjd1emd@naver.com>
2023-04-19 11:46:44 +0900

    Fix layout in LandingPage.tsx

--- src/features/pages/Landing/LandingPage.tsx
+++ src/features/pages/Landing/LandingPage.tsx
@@ -1,10 +1,13 @@
-import Card from "@/features/components/Card/Card";
+import Card from "@/components/Card";
 
 const LandingPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      project
-      <Card contents={'it\'s test'} isDone={false} />
+    <main className="min-h-screen w-screen">
+      <article className={'flex flex-col items-center gap-0.5 w-[395px] mx-auto my-0'}>
+        <span className={'font-bold-32 text-black pt-8'}>Hi! 🤚 Catze!</span>
+        <span className={'font-bold-20 text-black'}>2022.11.25</span>
+        <Card contents={'it\'s test'} isDone={false} />
+      </article>
     </main>
   )
 }

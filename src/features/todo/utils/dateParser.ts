a6d58324311a251f6237a02032d547de2d85dee1
howdyfrom2019 <tjd1emd@naver.com>
2023-04-20 12:21:51 +0900

    Fix useTimeHooks with time-series

--- src/features/todo/utils/dateParser.ts
+++ src/features/todo/utils/dateParser.ts
@@ -2,5 +2,13 @@ export const dateParser = (date: Date): [string, string] => {
   const [y, m, d] = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
   const [h, min, sec] = [date.getHours(), date.getMinutes(), date.getSeconds()];
 
-  return [`${y}.${m}.${d}.`, `${h}:${min}:${sec}`];
+  return [
+    `${y}.` +
+    `${('0').repeat(2 - String(m).length)}${m}.` +
+    `${('0').repeat(2 - String(d).length)}${d}.`,
+
+    `${('0').repeat(2 - String(h).length)}${h}:` +
+    `${('0').repeat(2 - String(min).length)}${min}:` +
+    `${('0').repeat(2 - String(sec).length)}${sec}`
+  ];
 }

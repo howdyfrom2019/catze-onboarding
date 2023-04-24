d19e005502b3f6df5f82c3cc91db4b65c9877653
howdyfrom2019 <tjd1emd@naver.com>
2023-04-24 10:53:43 +0900

    Replace ternary operator with clsx class

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,9 +1,10 @@
 import { ComponentPropsWithRef } from "react";
+import clsx from "clsx";
 
 const Input: React.FC<ComponentPropsWithRef<"input">> = ({ className, placeholder, ref, ...props }) => {
   return (
     <input
-      className={`rounded-s border-[1px] border-gray2 bg-white placeholder-textGray hover:border-blue3 focus:border-blue3 outline-0 p-2 ${className}`}
+      className={clsx('rounded-s border-[1px] border-gray2 bg-white placeholder-textGray hover:border-blue3 focus:border-blue3 outline-0 p-2', className && className)}
       placeholder={placeholder || 'Enter your task.'}
       {...props}
       ref={ref}

134d4cda2eb3c867692073833ef3996a8e867ae2
Jaake <49380742+howdyfrom2019@users.noreply.github.com>
2023-04-24 10:54:18 +0900

    [WIP] Refactor className with clsx (#20)
    * Add clsx to project
    
    * Replace ternary operator with clsx class

--- src/features/todo/components/Card/Card.tsx
+++ src/features/todo/components/Card/Card.tsx
@@ -2,6 +2,7 @@ import Check from "../../../../../public/icons/check-circle.svg";
 import CheckFilled from "../../../../../public/icons/check-circle-checked.svg";
 import useTodoList from '@/states/hooks/useTodoList';
 import React, { useCallback } from 'react';
+import clsx from 'clsx';
 
 interface CardProps {
   id: string;
@@ -22,7 +23,7 @@ const Card = ({ id, contents, createdAt, isDone }: CardProps) => {
 
   return (
     <div className={'flex justify-between items-center rounded-md border-gray2 border-[1px] w-full overflow-hidden'}>
-      <div className={`flex flex-col flex-1 gap-2 p-2 ${isDone ? 'bg-green1' : 'bg-white'}`}>
+      <div className={clsx('flex flex-col flex-1 gap-2 p-2', isDone ? 'bg-green1' : 'bg-white')}>
         <span className={'font-bold-16 text-black'}>{contents}</span>
         <span className={'font-bold-12 text-textGray'}>{createdAt}</span>
       </div>
